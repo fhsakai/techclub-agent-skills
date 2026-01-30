f9216d3e254be70e61569de3e6cbe47d4e9992ed
Felipe Rodrigues <me@felipefr.dev>
2026-01-30 20:29:12 -0300

    refactor: update category constants for better organization

--- libs/core/src/lib/constants.ts
+++ libs/core/src/lib/constants.ts
@@ -1,8 +1,9 @@
 import type { CategoryInfo } from './types'
 
-export const SKILLS_CATEGORIES_FILE = 'skills/categories.json'
 export const SKILLS_ROOT_DIR = 'skills'
 export const DEFAULT_CATEGORY_ID = 'uncategorized'
+export const CATEGORY_FOLDER_PATTERN = /^\(([a-z][a-z0-9-]*)\)$/
+export const CATEGORY_METADATA_FILE = '_category.json'
 
 export const DEFAULT_CATEGORY: CategoryInfo = {
   id: DEFAULT_CATEGORY_ID,
